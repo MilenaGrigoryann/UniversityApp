0240170e5358ad7e23665ea9b53eb17461270764
MilenaGrigoryann <101062835+MilenaGrigoryann@users.noreply.github.com>
2022-07-08 14:31:33 +0400

    Add files via upload

---
+++ UniversityAppp.Services/ITeacherService.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+public interface ITeacherService
+{
+    IEnumerable<Teacher>? GetAll();
+    Teacher? Get(Guid id);
+    void Create(Teacher? teacher);
+    void Update(Teacher? teacher);
+    void Delete(Guid id);
+}
