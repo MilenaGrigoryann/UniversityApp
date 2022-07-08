7885d38b5e68fd24b0c301d5d7b612516b639bdf
MilenaGrigoryann <101062835+MilenaGrigoryann@users.noreply.github.com>
2022-07-08 14:30:50 +0400

    Add files via upload

---
+++ UniversityAppp.Models/Teacher.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+public class Teacher
+{
+    public Teacher()
+    {
+        Id = Guid.NewGuid();
+    }
+    public Guid Id { get; set; }
+    public string? FirstName { get; set; }
+    public string? LastName { get; set; }
+    public int Age { get; set; }
+}
